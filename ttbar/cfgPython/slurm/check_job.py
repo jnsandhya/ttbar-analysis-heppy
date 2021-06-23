8b45577bfa56c58d18dfb4d4116de31c3350ddd3
aurelien <a.carle@ipnl.in2p3.fr>
2021-06-23 16:14:33 +0200

    check_job update

--- ttbar/cfgPython/slurm/check_job.py
+++ ttbar/cfgPython/slurm/check_job.py
@@ -8,64 +8,87 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('directory', help='display your directory')
 #parser.add_argument('sample', help='display your sample')
-parser.add_argument('log', help='log')
+
+
+######################################################
+## init
+######################################################
 
 args = parser.parse_args()
 directory = args.directory
 #sample = args.sample
-log = args.log
 
 jobs = os.listdir(directory)
 
-'''
-for i in range(100):
-
-    direc = directory+'/MC_'+sample+'_Chunk'+str(i)
-    cmd = 'tail '+direc+'/slurm*.out -n 1'
-    output = subprocess.check_output(cmd, shell=True)
-    print direc+' -> '+output
-
-
-'''
-
 n_fai = 0
 n_suc = 0 
+n_run = 0
 n_tot = 0
 failed_submission = []
 
+
+######################################################
+## Functions
+######################################################
+
+
+def get_slurm_file(target):
+    slurm = []
+    for l in os.listdir(target):
+        if l.find('slurm-') != -1:
+            slurm.append(l)
+    slurm.sort()
+    return slurm[-1]
+
+def print_job(name, n, n_tot):
+    print name+' job : '+str(math.floor(float(n)/n_tot*100))+'% --> '+str(n)+'/'+str(n_tot) 
+
+######################################################
+## Test jobs
+######################################################
 
 for j in jobs:
     if j.find('Chunk') == -1 :
         continue
 
     n_tot += 1
-    if log == 'log':
-        cmd = 'tail '+directory+'/'+j+'/log.txt -n 10'
-    else:
-        cmd = 'tail '+directory+'/'+j+'/local.output -n 10'
+    target_dir = directory+'/'+j
+    slurm_file = get_slurm_file(target_dir)
+
+    cmd1 = 'tail -n 10 '+target_dir+'/log.txt'
+    cmd2 = 'tail -n 1  '+target_dir+'/'+slurm_file
+
     try:
-        output = subprocess.check_output(cmd, shell=True)
+        output1 = subprocess.check_output(cmd1, shell=True)
+        output2 = subprocess.check_output(cmd2, shell=True)
     except:
         print ' ### Failed submission '+j+' ###'
         continue
-        #failed_submission.append(directory+'/'+j)
 
-    if output == '':
-        print j+' ### Empty log'
-        n_fai += 1
-        failed_submission.append(directory+'/'+j)
+    if output2.find('sending the job directory back') != -1 :
+        if output1 == '':
+            print j+' ### Failed job'
+            n_fai += 1
+            failed_submission.append(target_dir)
+        else:
+            print j+' -> '+output1
+            n_suc += 1
     else:
-        print j+' -> '+output
-        n_suc += 1
+        n_run += 1
+
+print '\n ******************************************* '
+print_job(' failed', n_fai, n_tot)
+print_job(' succed', n_suc, n_tot)
+print_job('running', n_run, n_tot)
+print '******************************************* \n'
 
 
-print ''
-print 'failed job :'+str(math.floor(float(n_fai)/n_tot*100))+'% --> '+str(n_fai)+'/'+str(n_tot) 
-print 'succed job :'+str(math.floor(float(n_suc)/n_tot*100))+'% --> '+str(n_suc)+'/'+str(n_tot)
-print ''
+######################################################
+## Resubmit
+######################################################
 
 
-print ' ------------ '
+print ' ------------ \n'
 start_harvest = None
 while start_harvest not in ['y','n']:
     start_harvest = raw_input('Resubmit jobs ? [y/n]')
@@ -73,7 +96,7 @@ if start_harvest == 'y':
     print 'Resubmit !.'
     for sub in failed_submission:
         #print ' -> '+sub
-        cmd = 'cd '+sub+' && rm -rf */ && sbatch ./batchScript.sh'
+        cmd = 'cd '+sub+' && rm -rf */ && rm slurm-* && sbatch ./batchScript.sh'
         os.system(cmd)
 else:
     quit()
