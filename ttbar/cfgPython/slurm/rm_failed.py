5466456067577d5ce50207ac3f986232ed851c25
aurelien <a.carle@ipnl.in2p3.fr>
2021-06-29 14:24:39 +0200

    update scripts

--- ttbar/cfgPython/slurm/rm_failed.py
+++ ttbar/cfgPython/slurm/rm_failed.py
@@ -1,5 +1,7 @@
 import os
 import argparse
+import shutil
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('directory', help='display your directory')
@@ -16,7 +18,6 @@ for l in listdir:
 for l in removed_dir:
     listdir.remove(l)
 
-print listdir
 
 if directory[-1] != '/':
     directory += '/'
@@ -27,20 +28,27 @@ for l in listdir:
     if l[-1] != '/':
         l += '/'    
     direct = directory+l
+    
+    listing = os.listdir(direct+'/MCWeighter/')        
+    
+    if len(listing) == 0:
+        print listing
+        shutil.rmtree(direct)
 
-    cmd = '\n\
-    directories=$(ls '+direct+' | grep _Chunk | sort)\n\
-    for directory in ${directories}\n\
-    do\n\
-        #echo "${directory}"\n\
-        if find '+direct+'${directory}/MCWeighter/ -mindepth 1 -maxdepth 1 | read; then\n\
-            echo "Not Empty"\n\
-        else\n\
-            rm -rf '+direct+'${directory} \n\
-            echo "Empty" \n\
-        fi\n\
-    done'
+    #print direct
+    #cmd = '\n\
+    #directories=$(ls '+direct+' | grep _Chunk | sort)\n\
+    #for directory in ${directories}\n\
+    #do\n\
+    #    #echo "${directory}"\n\
+    #    if find '+direct+'${directory}/MCWeighter/ -mindepth 1 -maxdepth 1 | read; then\n\
+    #        echo "Not Empty"\n\
+    #    else\n\
+    #        rm -rf '+direct+'${directory} \n\
+    #        echo "Empty" \n\
+    #    fi\n\
+    #done'
 
     #print cmd
-    os.system(cmd)
+    #os.system(cmd)
 
