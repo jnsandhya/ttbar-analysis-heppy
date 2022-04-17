fbb82ea0971c5544620a9db89665f0127d61cd38
nicolas chanon <nicolas.pierre.chanon@cern.ch>
2022-04-17 09:45:39 +0200

    Further protection for GenInfo

--- ttbar/python/analyzers/MCWeighter.py
+++ ttbar/python/analyzers/MCWeighter.py
@@ -5,7 +5,12 @@ class MCWeighter(Analyzer):
 
     def declareHandles(self):
         super(MCWeighter, self).declareHandles()
-        self.mchandles['GenInfo'] = AutoHandle( ('generator','',''), 'GenEventInfoProduct' )
+        #self.mchandles['GenInfo'] = AutoHandle( ('generator','',''), 'GenEventInfoProduct' )
+        self.mchandles['GenInfo'] = AutoHandle('generator',
+                                                'GenEventInfoProduct',
+                                                 mayFail=True,
+                                                 fallbackLabel='source',
+                                                 lazy=False )
 
     def beginLoop(self, setup):
         super(MCWeighter,self).beginLoop(setup)
@@ -19,7 +24,9 @@ class MCWeighter(Analyzer):
         self.count.inc('All Events')
         if self.cfg_comp.isMC: 
             self.readCollections(event.input)
-            event.generatorWeight = self.mchandles['GenInfo'].product().weight()
+	    if self.mchandles['GenInfo'].isValid():
+                event.generatorWeight = self.mchandles['GenInfo'].product().weight()
+	    else:
+		event.generatorWeight = 0.
             self.count.inc('Sum Weights', event.generatorWeight)
 
-            
